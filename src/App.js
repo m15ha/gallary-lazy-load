cf43ae8b9d54dc546a956bb1f63eff19f5fcd77c
m15ha <mihail.lazareff@gmail.com>
2021-07-31 07:54:33 -0400

    galary API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const url =
+        'https://api.unsplash.com/photos/random?client_id=X9T6Nn9PKeXkpwjk9c1lLLyuLqtdzpVUlOXaUIIagrA&count=30';
+    const [images, setImages] = useState([]);
+    const getImages = () => {
+        Axios.get(url).then((res) => {
+            setImages(res.data);
+        });
+    };
+    useEffect(() => {
+        getImages();
+    }, []);
+
+    if (!images) {
+        return <h1>Loading</h1>;
+    }
+    return (
+        <div className='App'>
+            {images.map((image) => {
+                return (
+                    <LazyLoadImage
+                        effect='blur'
+                        src={image.urls.regular}
+                        alt={image.id}
+                        key={image.id}
+                        height='500px'
+                        weight='400px'
+                        placeholderSrc={process.env.PUBLIC_URL + '/logo192.png'}
+                    />
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
